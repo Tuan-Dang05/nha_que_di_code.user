b2c7dff114419cb2745b49e291d4681422247377
tuananh-05 <anhtuandang2605@gmail.com>
2024-04-23 22:06:55 +0700

    update: getToken.js

--- js/getToken.js
+++ js/getToken.js
@@ -1,18 +1,17 @@
 const getToken = () =>{
-    const saveAccessToken = window.localStorage.getItem('access_token');
-    if(saveAccessToken){
-        return saveAccessToken;
-    }else{
+    // const saveAccessToken = window.localStorage.getItem('access_token');
+    // if(saveAccessToken){
+    //     return saveAccessToken;
+    // }else{
 
         const url = new URLSearchParams(window.location.hash.substring(1))
         const token = url.get("access_token")
-        window.localStorage.setItem('access_token',token);
+        // window.localStorage.setItem('access_token',token);
         return token
-    }
+    // }
 }
 const getUserInfo = async () => {
     // test access_token 
-    // 'ya29.a0Ad52N3-lkl2oIT22Cnjs2Al9qDZ-hm_FyizoXyqdRWDGYBeTWSuYIkIB3Xq5rdcMmKLQx6869VLcc5fRa7Pu0P_2XdgPp31L5BNysvK5bcfE4vsTEDRAjiSG9qP2B69irHv4PVG7aCz6O4yPztH7P0ih7TJrgDaqWxgaCgYKAc0SARESFQHGX2MikzB_4vTr5-Ag_jL8Pm6vEQ0170&token_type=Bearer&expires_in=3599&scope=email%20profile%20https://www.googleapis.com/auth/userinfo.email%20openid%20https://www.googleapis.com/auth/userinfo.profile&authuser=0&prompt=none'
     const access_token = getToken();
     const respone = await fetch(
         `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
